9337224da43b1c480e744700bf87a8422a122177
Edgar21AB <134448114+Edgar212004@users.noreply.github.com>
2023-11-03 14:45:44 -0600

    creacion proyecto

---
+++ ProyectoFinalG4/src/main/java/ProyectoFinalG4/demo/ProyectoFinalG4Application.java
@@ -0,0 +1,13 @@
+package ProyectoFinalG4.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalG4Application.class, args);
+	}
+
+}
